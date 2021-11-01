0b0e3c605d591e97ae87cf33c439ce3e564dbec9
minjib <48316078+minjib@users.noreply.github.com>
2021-11-01 20:08:15 +0900

    init

---
+++ src/test/java/com/koscom/springboot/CalculateTest.java
@@ -0,0 +1,12 @@
+package com.koscom.springboot;
+
+import org.junit.jupiter.api.Test;
+
+public class CalculateTest {
+
+    @Test
+    public void add() {
+        long sum = 1 + 2;
+        //assertThat();
+    }
+}
